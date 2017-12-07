a35f8550a6ecfbbd582c6d0b56fbb11a9e9bb10e
Leonardo Souza Oliveira <Leonardo.oliveira@paxbr.com.br>
2017-12-07 15:44:27 -0200

    first commit

---
+++ js/custom.js
@@ -0,0 +1,56 @@
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+});
+
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+$(document).ready(function() {
+
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+  function initParallax() {
+    $('#intro').parallax("100%", 0.1);
+    $('#overview').parallax("100%", 0.3);
+    $('#detail').parallax("100%", 0.2);
+    $('#register').parallax("100%", 0.1);
+  }
+
+  initParallax();
+
+  $(document).ready(function() {
+    $("#owl-speakers").owlCarousel({
+      autoPlay: 6000,
+      items : 4,
+      itemsDesktop : [1199,2],
+      itemsDesktopSmall : [979,1],
+      itemsTablet: [768,1],
+      itemsTabletSmall: [985,2],
+      itemsMobile : [479,1],
+    });
+  });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+        $('.go-top').fadeIn(200);
+        } else {
+          $('.go-top').fadeOut(200);
+        }
+        });   
+        // Animate the scroll to top
+      $('.go-top').click(function(event) {
+        event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 300);
+      })
+
+  new WOW({ mobile: false }).init();
+
+  });
+
